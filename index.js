66e74c99e5efb00be624baf49cb835986140b65e
Daniel Jiménez Rivera <dani@internxt.com>
2026-04-10 10:15:39 +0200

    Add end of line "lf"

---
+++ index.js
@@ -0,0 +1,11 @@
+/**
+ * @see https://prettier.io/docs/configuration
+ * @type {import("prettier").Config}
+ */
+module.exports = {
+  semi: true,
+  trailingComma: "all",
+  singleQuote: true,
+  printWidth: 120,
+  tabWidth: 2,
+};
